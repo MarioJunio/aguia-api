dcc4579aea5ee1a6ae237776cbcc651361e89ccb
Mario Marques Martins <mmarquma@everis.com>
2019-07-29 10:58:51 -0300

    first commit

---
+++ src/main/java/br/com/aguia/AguiaApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.aguia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AguiaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AguiaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/aguia/AguiaApiApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.aguia;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AguiaApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
